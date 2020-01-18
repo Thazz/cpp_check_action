d095330d2d390a66ee466647700a6821393ef081
Thazz <gregor.seljak@gmail.com>
2020-01-18 23:17:51 +0100

    dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Branch name: $BRANCH"
+
 FILES=$(find -regex '.*/.*\.\(c\|cpp\|yml\)$')
 echo "Listing files in directory:"
+echo "$FILES"
+
+FILES=$(git ls-files -m)
+echo "Modified files (git ls-files):"
+echo "$FILES"
+
+FILES=$(git diff --name-only --diff-filter=b $(git merge-base HEAD experimental))
+echo "Modified files (git diff):"
 echo "$FILES"
 
 echo "Event path:"
