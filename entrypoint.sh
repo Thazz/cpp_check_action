5d9af2a75be65598030870c9e5e5a6cfd8a8bab3
Thazz <gregor.seljak@gmail.com>
2020-01-19 21:24:37 +0100

    dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo "command: git fetch origin"
+git fetch origin
+
+echo "command: git ls-files"
+git ls-files
+
+echo "command: git branch"
+gir branch
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Branch name: $BRANCH"
@@ -8,7 +16,7 @@ echo "Listing files in directory:"
 echo "$FILES"
 
 FILES=$(git ls-files -m)
-echo "Modified files (git ls-files):"
+echo "Modified files (git ls-files -m):"
 echo "$FILES"
 
 FILES=$(git diff --name-only --diff-filter=b $(git merge-base HEAD experimental))
