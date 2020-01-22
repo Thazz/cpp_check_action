4647b851c18e1d1f4338b94ef91d967b8d6519ee
Thazz <gregor.seljak@gmail.com>
2020-01-22 13:21:14 +0100

    color output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-PEDANTIC=0
+function color_output() {
+    local c=$1
+    shift
+    echo -e "\e[${c}m$*\e[0m"
+}
+
+FAIL_ON_WARN=0
+FAIL_DUE_ERR=1
+FAIL_DUE_WARN=2
 
 CPP_CHECK_OUTPUT="cppcheck.out"
 CPP_CHECK_ERROR="cppcheck.out.error"
@@ -12,8 +20,7 @@ CPP_CHECK_PERF="cppcheck.out.perf"
 CPP_CHECK_PORT="cppcheck.out.port"
 CPP_CHECK_INFO="cppcheck.out.info"
 
-echo ""
-echo "::group::Run cppcheck on files"
+echo "::group::Run cppcheck"
 cppcheck --std=c++11 --output-file=$CPP_CHECK_OUTPUT --language=c++ --enable=all ./
 echo "::endgroup::"
 
@@ -31,7 +38,6 @@ NUM_PERF=$(cat $CPP_CHECK_PERF | wc -l)
 NUM_PORT=$(cat $CPP_CHECK_PORT | wc -l)
 NUM_INFO=$(cat $CPP_CHECK_INFO | wc -l)
 
-echo ""
 echo "::group::Cppcheck summary:"
 echo "  $NUM_ERROR errors"
 echo "  $NUM_WARN warnings"
@@ -43,47 +49,57 @@ echo "::endgroup::"
 
 RS=0
 if [[ NUM_ERROR -gt 0 ]]; then
-    echo ""
-    echo "::group::::error::Errors:"
+    color_output $ANSI_RED "::group::Errors:"
     while IFS= read -r line; do
         echo "::error::$line"
     done <$CPP_CHECK_ERROR
     echo "::endgroup::"
-    #awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_ERROR
-    RS=1
+
+    RS=$FAIL_DUE_ERR
 fi
 
 if [[ $NUM_WARN -gt 0 ]]; then
-    echo ""
-    echo "Warnings:"
-    awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_WARN
-    if [[ PEDANTIC -gt 0 ]]; then
-        RS=1
+    color_output $ANSI_YELLOW "::group::Warnings:"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <$CPP_CHECK_WARN
+    echo "::endgroup::"
+
+    if [[ FAIL_ON_WARN -gt 0 ]]; then
+        RS=$FAIL_DUE_WARN
     fi
 fi
 
 if [[ $NUM_STYLE -gt 0 ]]; then
-    echo ""
-    echo "Style Warnings:"
-    awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_STYLE
+    color_output $ANSI_YELLOW "::group::Style Warnings:"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <$CPP_CHECK_STYLE
+    echo "::endgroup::"
 fi
 
 if [[ $NUM_PERF -gt 0 ]]; then
-    echo ""
-    echo "Performance Warnings:"
-    awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_STYLE
+    color_output $ANSI_YELLOW "::group::Performance Warnings:"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <$CPP_CHECK_PERF
+    echo "::endgroup::"
 fi
 
 if [[ $NUM_PORT -gt 0 ]]; then
-    echo ""
-    echo "Portability Warnings:"
-    awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_STYLE
+    color_output $ANSI_YELLOW "::group::Portability Warnings:"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <$CPP_CHECK_PORT
+    echo "::endgroup::"
 fi
 
 if [[ $NUM_INFO -gt 0 ]]; then
-    echo ""
-    echo "Information messages:"
-    awk '{printf "%d\t%s\n", NR, $0}' <$CPP_CHECK_STYLE
+    color_output $ANSI_GREEN "::group::Portability Warnings:"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <$CPP_CHECK_INFO
+    echo "::endgroup::"
 fi
 
 exit $RS
