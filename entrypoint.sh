7d9a990e89319dff8ab97354fded5cfb23e1939f
Thazz <gregor.seljak@gmail.com>
2020-01-23 13:58:51 +0100

    Refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,38 @@
 set -euo pipefail
 
 function color_output() {
-    local c=$1
-    shift
-    echo -e "\e[${c}m$*\e[0m"
+    local MESSAGE=$1
+    local COLOR=$2
+    local MOD=$3
+
+    echo -e "${MOD}\e[${COLOR}m${MESSAGE}\e[0m"
 }
 
+function print_summary() {
+    local MSG=$1
+    local FILE=$2
+    local COLOR=$3
+    local NUM
+
+    NUM=$(wc -l "$FILE")
+    local HEADER_MSG="$MSG ($NUM):"
+
+    color_output "$HEADER_MSG" "$COLOR" "::group::"
+    while IFS= read -r line; do
+        echo "::warn::$line"
+    done <"$FILE"
+    echo "::endgroup::"
+}
+
+# Log colors
 ANSI_RED=31
 ANSI_GREEN=32
 ANSI_YELLOW=33
 
+# Treat warnings as error
 FAIL_ON_WARN=0
-FAIL_DUE_ERR=1
-FAIL_DUE_WARN=2
 
+# Files for parsing output
 CPP_CHECK_OUTPUT="cppcheck.out"
 CPP_CHECK_ERROR="cppcheck.out.error"
 CPP_CHECK_WARN="cppcheck.out.warn"
@@ -35,76 +54,30 @@ grep '(performance)' $CPP_CHECK_OUTPUT >$CPP_CHECK_PERF || true
 grep '(portability)' $CPP_CHECK_OUTPUT >$CPP_CHECK_PORT || true
 grep '(information)' $CPP_CHECK_OUTPUT >$CPP_CHECK_INFO || true
 
-NUM_ERROR=$(cat $CPP_CHECK_ERROR | wc -l)
-NUM_WARN=$(cat $CPP_CHECK_WARN | wc -l)
-NUM_STYLE=$(cat $CPP_CHECK_STYLE | wc -l)
-NUM_PERF=$(cat $CPP_CHECK_PERF | wc -l)
-NUM_PORT=$(cat $CPP_CHECK_PORT | wc -l)
-NUM_INFO=$(cat $CPP_CHECK_INFO | wc -l)
+print_summary "Errors" $CPP_CHECK_ERROR $ANSI_RED
+print_summary "Warnings" $CPP_CHECK_WARN $ANSI_YELLOW
+print_summary "Style Warnings" $CPP_CHECK_WARN $ANSI_YELLOW
+print_summary "Performance Warnings" $CPP_CHECK_WARN $ANSI_YELLOW
+print_summary "Portability Warnings" $CPP_CHECK_WARN $ANSI_YELLOW
+print_summary "Info Messages" $CPP_CHECK_INFO $ANSI_GREEN
 
-echo "::group::Cppcheck summary:"
-echo "  $NUM_ERROR errors"
-echo "  $NUM_WARN warnings"
-echo "  $NUM_STYLE style warnings"
-echo "  $NUM_PERF performance warnings"
-echo "  $NUM_PORT portability warnings"
-echo "  $NUM_INFO information messages"
-echo "::endgroup::"
+NUM_ERROR=$(wc -l <$CPP_CHECK_ERROR)
+NUM_WARN=$(wc -l <$CPP_CHECK_WARN)
+NUM_STYLE=$(wc -l <$CPP_CHECK_STYLE)
+NUM_PERF=$(wc -l <$CPP_CHECK_PERF)
+NUM_PORT=$(wc -l <$CPP_CHECK_PORT)
+# shellcheck(SC2086)
+NUM_TOTAL_WARN=$NUM_WARN + $NUM_STYLE + $NUM_PERF + $NUM_PORT
 
 RS=0
-if [[ NUM_ERROR -gt 0 ]]; then
-    #color_output $ANSI_RED "::group::Errors:"
-    echo -e "::group::\e[${ANSI_RED}mErrors:\e[0m"
-    while IFS= read -r line; do
-        echo "::error::$line"
-    done <$CPP_CHECK_ERROR
-    echo "::endgroup::"
-
-    RS=$FAIL_DUE_ERR
-fi
-
-if [[ $NUM_WARN -gt 0 ]]; then
-    color_output $ANSI_YELLOW "::group::Warnings:"
-    while IFS= read -r line; do
-        echo "::warn::$line"
-    done <$CPP_CHECK_WARN
-    echo "::endgroup::"
-
-    if [[ FAIL_ON_WARN -gt 0 ]]; then
-        RS=$FAIL_DUE_WARN
-    fi
-fi
-
-if [[ $NUM_STYLE -gt 0 ]]; then
-    color_output $ANSI_YELLOW "::group::Style Warnings:"
-    while IFS= read -r line; do
-        echo "::warn::$line"
-    done <$CPP_CHECK_STYLE
-    echo "::endgroup::"
-fi
-
-if [[ $NUM_PERF -gt 0 ]]; then
-    color_output $ANSI_YELLOW "::group::Performance Warnings:"
-    while IFS= read -r line; do
-        echo "::warn::$line"
-    done <$CPP_CHECK_PERF
-    echo "::endgroup::"
-fi
-
-if [[ $NUM_PORT -gt 0 ]]; then
-    color_output $ANSI_YELLOW "::group::Portability Warnings:"
-    while IFS= read -r line; do
-        echo "::warn::$line"
-    done <$CPP_CHECK_PORT
-    echo "::endgroup::"
-fi
-
-if [[ $NUM_INFO -gt 0 ]]; then
-    color_output $ANSI_GREEN "::group::Information Messagess:"
-    while IFS= read -r line; do
-        echo "::warn::$line"
-    done <$CPP_CHECK_INFO
-    echo "::endgroup::"
+if [[ $NUM_ERROR -gt 0 ]]; then
+    echo "::error::Check failed! Errors found."
+    RS=1
+elif [[ $FAIL_ON_WARN -ne 0 && $NUM_TOTAL_WARN -gt 0 ]]; then
+    echo "::error::Check failed! Warnings found which are treated as errors."
+    RS=1
+else
+    echo "Check passed!"
 fi
 
 exit $RS
